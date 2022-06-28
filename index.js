95d37dd8eebb3f86aa00303b4cb70768e7ac9326
Alex <friedmanalexg@gmail.com>
2022-06-28 11:29:31 -0400

    commit tree 824c788ba22db0732278d269e804d837c6195ed8

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const testVar = "hi"
+
+/* Be a lot cooler if this was explained on the lab instead of that "remove child" command */
+document.querySelector("main#main").remove();
+
+/*  */
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.textContent = "Alex is the champion!";
